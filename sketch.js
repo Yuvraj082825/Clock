aaee4069b427382854cece1ca58841d7bf9c3457
Yuvraj082825 <73472779+Yuvraj082825@users.noreply.github.com>
2021-02-03 16:40:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var hr,min,sec
+var h,m,s;
+function setup()
+{
+  createCanvas(500,500);
+  angleMode(DEGREES)
+}
+function draw() {
+  background(0);  
+  translate(200,200)
+  rotate(-90)
+  hr=hour()
+  min=minute()
+  sec=second()
+  
+  s=map(sec,0,60,0,360)
+  m=map(min,0,60,0,360)
+  h=map(hr%12,0,12,0,360)
+
+  push()
+  rotate(s)
+  stroke(255,0,0)
+  strokeWeight(7)
+  line(0,0,100,0)
+  pop()
+
+  push()
+  rotate(m)
+  stroke(0,255,0)
+  strokeWeight(7)
+  line(0,0,75,0)
+  pop()
+
+  push()
+  rotate(h)
+  stroke(0,0,255)
+  strokeWeight(7)
+  line(0,0,50,0)
+  pop()
+
+  stroke(255,0,255)
+  point(0,0)
+  strokeWeight(10)
+  noFill()
+
+  stroke(255,0,0)
+  arc(0,0,300,300,0,s)
+
+  stroke(0,255,0)
+  arc(0,0,280,280,0,m)
+
+  stroke(0,0,255)
+  arc(0,0,260,260,0,h)
+}
